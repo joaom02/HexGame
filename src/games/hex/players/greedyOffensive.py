dc0f81071a51d1e282a141b1b8034297f9099ad7
João Martins <103424273+joaom02@users.noreply.github.com>
2023-05-04 11:08:26 +0100

    p

--- src/games/hex/players/greedyOffensive.py
+++ src/games/hex/players/greedyOffensive.py
@@ -4,15 +4,12 @@ from games.hex.player import HexPlayer
 from games.hex.state import HexState
 from games.state import State
 
-
 class GreedyOHexPlayer(HexPlayer):
-
     def __init__(self, name):
         super().__init__(name)
 
     def get_action(self, state: HexState):
         grid = state.get_grid()
-
         selected_col = None
         selected_row = None
         max_count = 0
@@ -22,17 +19,15 @@ class GreedyOHexPlayer(HexPlayer):
                 count = 0
                 if not state.validate_action(HexAction(col, row)):
                     continue
-
                 if grid[row][col] == self.get_current_pos():
                     count += row + col
-
-            # it swap the column if we exceed the count. if the count of chips is the same, we swap 50% of the times
                 if selected_col is None or selected_row is None or count > max_count or (count == max_count and choice([False, True])):
                     selected_col = col
                     selected_row = row
                     max_count = count
-
         if selected_col is None:
+            raise Exception("There is no valid action")
+        if selected_row is None:
             raise Exception("There is no valid action")
 
         return HexAction(selected_col, selected_row)
